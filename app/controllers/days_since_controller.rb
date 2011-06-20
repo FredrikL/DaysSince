e2d6520dc3924c7ab5a58aa9ed2baa8b0b1ef652
Fredrik Leijon <fredrik.leijon@gmail.com>
2011-06-20 21:28:46 +0200

    Working json rendering, no xml yet

--- app/controllers/days_since_controller.rb
+++ app/controllers/days_since_controller.rb
@@ -7,12 +7,20 @@ class DaysSinceController  < ApplicationController
     day = params[:day].to_i
     time = DateTime.new(year, month, day)
 
-    @result = (DateTime.now-time).to_i
+    @result = DaysSince.new((DateTime.now-time).to_i)
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml =>  @result }
+      #format.xml  { render :xml =>  @result.to_xml }
       format.json { render :json => @result }
+    end
+  end
+
+  class DaysSince
+    attr_accessor :days
+
+    def initialize(days)
+      self.days = days
     end
   end
 end
