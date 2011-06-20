a0f21f503d09e56e25c93560a4f6859f6f5ecb3f
Fredrik Leijon <fredrik.leijon@gmail.com>
2011-06-20 21:11:12 +0200

    Logic working, with html rendering

--- app/controllers/days_since_controller.rb
+++ app/controllers/days_since_controller.rb
@@ -1,3 +1,18 @@
-class DaysSinceController
-  # To change this template use File | Settings | File Templates.
+require 'date'
+
+class DaysSinceController  < ApplicationController
+  def show
+    year = params[:year].to_i
+    month = params[:month].to_i
+    day = params[:day].to_i
+    time = DateTime.new(year, month, day)
+
+    @result = (DateTime.now-time).to_i
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml =>  @result }
+      format.json { render :json => @result }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,6 @@ DaysSince::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
+  match "/:year/:month/:day" => "days_since#show"
 end
