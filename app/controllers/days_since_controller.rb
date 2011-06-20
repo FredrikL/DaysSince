fa5df80cbd01c28e92c934dd76a9cf80510dd719
Fredrik Leijon <fredrik.leijon@gmail.com>
2011-06-20 21:46:02 +0200

    meh

--- app/controllers/days_since_controller.rb
+++ app/controllers/days_since_controller.rb
@@ -1,26 +1,26 @@
 require 'date'
 
 class DaysSinceController  < ApplicationController
+  class DaysSinceResult # should live in models..
+    attr_accessor :days
+
+    def initialize(days)
+      self.days = days
+    end
+  end
+
   def show
     year = params[:year].to_i
     month = params[:month].to_i
     day = params[:day].to_i
     time = DateTime.new(year, month, day)
 
-    @result = DaysSince.new((DateTime.now-time).to_i)
+    @result = DaysSinceResult.new((DateTime.now-time).to_i)
 
     respond_to do |format|
       format.html # show.html.erb
       #format.xml  { render :xml =>  @result.to_xml }
       format.json { render :json => @result }
-    end
-  end
-
-  class DaysSince
-    attr_accessor :days
-
-    def initialize(days)
-      self.days = days
     end
   end
 end
